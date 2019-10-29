92241a80dc7d561cfb7eacc36c99720e482dea51
JasonBiao <13974020621@163.com>
2019-10-29 17:37:18 +0800

    'test'

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tensorflow学习
+> python 版本 3.7.3
+## 安装
+pip install tensorflow
+pip install numpy
+pip install pandas
+pip install matplotlib
---
+++ test/tensorflowVer.py
@@ -0,0 +1,4 @@
+import os;
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2';
+import tensorflow as tf;
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
